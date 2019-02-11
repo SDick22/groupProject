2f06123b3d5ed8acb1dcd49fa94b8fbee4691e15
SAD22 <SAD22@FSTU603019.pti.edu>
2019-02-11 13:47:47 -0500

    Adding a commit message

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,13 @@
+
+package javaapplication3;
+
+public class JavaApplication3 {
+
+    public static void main(String[] args) {
+        Sam();
+    }
+    public static void Sam(){
+        System.out.println("Here is my edit");
+    }
+    
+}
